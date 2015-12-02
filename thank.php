2eeb803a16aef44a93340eb3996ce8ba3f2b6c9b
syuwared <swtennisred@gmail.com>
2015-12-02 23:51:18 +0000

    initial commit

---
+++ thank.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>入力フォーム</title>
+  </head>
+	<body>
+  <table>
+    <tr>
+      <th>氏名</th>
+      <td><?php print( htmlspecialchars( $_REQUEST["name"], ENT_QUOTES)); ?></td>
+    </tr>
+
+     <tr>
+       <th>入力フォーム</th>
+       <td><?php print( htmlspecialchars( $_REQUEST["area"], ENT_QUOTES)); ?></td>
+    </tr>
+  </table>
+	</body>
+</html>
+
